b9749201881fedf564cb11a33ccad15721c62713
Francesco <108478877+fraro01@users.noreply.github.com>
2024-07-09 16:23:37 +0200

    Sincronizzazione al 09/07/2024

--- Romucci_script.js
+++ Romucci_script.js
@@ -1,14 +1,14 @@
 //funzione JS per poter oscurare le celle della tabella sul "mouseover"
 function mouseOver(element) {                            
-    element.style.backgroundColor = "#28282B";
-    //document.getElementById(oggetto).style.border = "1px solid white";
-  }
+        element.style.backgroundColor = "#28282B";
+        //document.getElementById(oggetto).style.border = "1px solid white";
+      }
 //funzione per ripristinare il colore base una volta tolto il mouse
-  function mouseOut(element) {
-    element.style.backgroundColor = "";
-  }
+function mouseOut(element) {
+        element.style.backgroundColor = "";
+        }
 
 // per la navigazione tra le pagine
 function navigateTo(url) {
-  window.location.href = url;
-}
+        window.location.href = url;
+      }
